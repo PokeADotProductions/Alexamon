132fca91b41248ee292dfd92669c2c03454d03e6
RyTaus <rtaus1@lion.lmu.edu>
2016-11-06 14:51:17 -0800

    added subject

--- src/Adventure/index.js
+++ src/Adventure/index.js
@@ -1,89 +1,3 @@
-// var numToLet = [", A ", ", B ", ", C ", ", D "]
-// var letToNum = {a: 0, b: 1, c: 2, d: 3}
-
-// var Branch = function(hint, flavor, branches, isWinner) {
-//     this.hint = hint;
-//     this.text = flavor;
-//     this.branches = branches;
-//     this.isWinner = isWinner;
-//     this.invalidChoice = false;
-
-//   this.goDownBranch = function(index) {
-//     if (this.branches.length <= index) {
-//       console.log("Invalid index");
-//       this.invalidChoice = true;
-//       return this;
-//     } else {
-//       return this.branches[index];
-//     }
-//   }
-
-//   this.isLeaf = function() {
-//     this.branches.length === 0;
-//   }
-
-//   this.addBranch = function(branch) {
-//     this.branches.push(branch);
-//   }
-
-//   this.getDescription = function() {
-//     var str = this.text + "  Do you";
-
-//     if (this.invalidChoice) {
-//       str = "I don't believe that was an option.  " + str;
-//       this.invalidChoice = false;
-//     }
-
-//     if (this.isLeaf()) {
-//       console.log("        END");
-//       return this.text;
-//     }
-
-//     for (var i = 0; i < this.branches.length; i ++) {
-//       if (i == this.branches.length - 1) {
-//         str = str + ", or"
-//       }
-//       str = str + numToLet[i] + this.branches[i].hint;
-//     }
-//     return str;
-//   }
-// }
-
-
-// var makeBranch = function (hint, flavor) {
-//   return new Branch(hint, flavor, [], false)
-// }
-
-// var makeLeaf = function (hint, flavor, isWinner) {
-//   return new Branch(hint, flavor, [], isWinner)
-// }
-
-// var b = makeBranch("", "You come across a pikachu.")
-
-// // console.log( new Branch("hint", "flavor", [], false, false));
-
-// b.addBranch(makeLeaf("pick it up", "the pikachu shocks you and you die", false))
-//   var leaveIt = makeBranch("walk away", "it follows you to the pokemon center.")
-//   b.addBranch(leaveIt);
-//     var healit = makeBranch("heal it", "it now trusts you and joins your team.")
-//     leaveIt.addBranch(healit)
-//     leaveIt.addBranch(makeLeaf("kick it", "it dies and you win the world", true))
-
-// var currBranch = b;
-// console.log(currBranch);
-// console.log(currBranch.getDescription());
-
-// currBranch = currBranch.goDownBranch(1)
-
-// console.log(currBranch);
-// console.log(currBranch.getDescription())
-
-// currBranch.goDownBranch(7);
-
-// console.log(currBranch);
-// console.log(currBranch.getDescription())
-
-
 
 //----------------------Alexa---------------------------------
 
@@ -93,13 +7,15 @@
 exports.handler = function( event, context ) {
   var numToLet = [", A ", ", B ", ", C ", ", D "]
   var letToNum = {a: 0, b: 1, c: 2, d: 3}
+  var subjects = {}
 
-  var Branch = function(hint, flavor, branches, isWinner) {
+  var Branch = function(hint, flavor, branches, isWinner, subject) {
     this.hint = hint;
     this.text = flavor;
     this.branches = branches;
     this.isWinner = isWinner;
     this.invalidChoice = false;
+    this.subject = subject;
 
   this.goDownBranch = function(index) {
     if (this.branches.length <= index) {
@@ -117,6 +33,15 @@ exports.handler = function( event, context ) {
 
   this.addBranch = function(branch) {
     this.branches.push(branch);
+  }
+
+  this.getSubjectDescription = function() {
+    if (this.subject == null) {
+      return "nothing to inspect."
+    }
+     // do something
+}
+    return subjects[this.subject.toLowerCase()]
   }
 
   this.getDescription = function() {
@@ -143,24 +68,24 @@ exports.handler = function( event, context ) {
 }
 
 
-  var makeBranch = function (hint, flavor) {
-  return new Branch(hint, flavor, [], false)
+  var makeBranch = function (hint, flavor, subject) {
+  return new Branch(hint, flavor, [], false, subject)
   }
 
-  var makeLeaf = function (hint, flavor, isWinner) {
-  return new Branch(hint, flavor, [], isWinner)
+  var makeLeaf = function (hint, flavor, isWinner, subject) {
+  return new Branch(hint, flavor, [], isWinner, subject)
   }
 
-  var b = makeBranch("", "You come across a pikachu.")
+  var b = makeBranch("", "You come across a pikachu.", "bulbasaur")
 
   // console.log( new Branch("hint", "flavor", [], false, false));
 
-  b.addBranch(makeLeaf("pick it up", "the pikachu shocks you and you die", false))
-  var leaveIt = makeBranch("walk away", "it follows you to the pokemon center.")
+  b.addBranch(makeLeaf("pick it up", "the pikachu shocks you and you die", false, null))
+  var leaveIt = makeBranch("walk away", "it follows you to the pokemon center.", null)
   b.addBranch(leaveIt);
-    var healit = makeBranch("heal it", "it now trusts you and joins your team.")
+    var healit = makeBranch("heal it", "it now trusts you and joins your team.", null)
     leaveIt.addBranch(healit)
-    leaveIt.addBranch(makeLeaf("kick it", "it dies and you win the world", true))
+    leaveIt.addBranch(makeLeaf("kick it", "it dies and you win the world", true, null))
 
   var currBranch = b;
   console.log(currBranch);
@@ -195,6 +120,10 @@ exports.handler = function( event, context ) {
 
             var response = event.request.intent.slots.response.value;
             response = letToNum[response];
+            if (response == null) {
+              say = currBranch.getSubjectDescription();
+              context.succeed({sessionAttributes: sessionAttributes, response: buildSpeechletResponse(say, shouldEndSession) });
+            }
             currBranch = currBranch.goDownBranch(response);
             say = currBranch.getDescription();
 
